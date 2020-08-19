0fb0da34e3c52a8a5c09ad2a481b2032ca5f7f79
zwarag <harrys.kavan@protonmail.com>
2020-08-19 16:45:34 +0200

    refactor(all): move frontend to it's own package
    refactor(rest): move rest to to the frontend application

--- packages/foo/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of foo
- */
-
-export * from './lib/foo.service';
-export * from './lib/foo.component';
-export * from './lib/foo.module';
---
+++ packages/frontend/src/rest.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import {map, tap} from 'rxjs/operators';
+
+import { FederalState } from './federalStates';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getFederalStates(): Observable<FederalState[]> {
+    const url = `https://services1.arcgis.com/YfxQKFk1MjjurGb5/ArcGIS/rest/services/AUSTRIA_COVID19_Cases/FeatureServer/3/query?where=datum >= CURRENT_TIMESTAMP -30&outFields=*&sqlFormat=none&f=pjson`;
+    return this.http.get<any>(url)
+      .pipe(
+        tap(e => console.log(e)),
+        map(e => e.features.map(x => x.attributes)),
+        tap(e => console.log(e))
+      );
+  }
+}
