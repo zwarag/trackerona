23b1b45b13a6c8702be18701a487c3998d2a45d5
zwarag <harrys.kavan@protonmail.com>
2020-08-20 20:30:16 +0200

    feat(rest): split up timeline and latest data

--- packages/frontend/src/app/data.service.ts
+++ packages/frontend/src/app/data.service.ts
@@ -3,11 +3,15 @@ import {RestService} from './rest.service';
 import {Observable} from 'rxjs';
 import {FederalState} from './federalState';
 import {FederalDistrict} from './federalDistrict';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+
+  readonly NFederalStates: number = 9;
+  readonly NFederalDistricts: number = 94;
 
   federalStates$: Observable<FederalState[]>;
   federalDistricts$: Observable<FederalDistrict[]>;
@@ -19,11 +23,29 @@ export class DataService {
     this.federalDistricts$ = this.rest.getFederalDistricts();
   }
 
+  /*
+   * Returns the only the newest Information
+   */
   getFederalStates(): Observable<FederalState[]> {
+    return this.federalStates$.pipe(
+      map(x => x.slice(-this.NFederalStates))
+    );
+  }
+
+  getFederalStatesTimeLine(): Observable<FederalState[]> {
     return this.federalStates$;
   }
 
+  /*
+   * Returns the only the newest Information
+   */
   getFederalDistricts(): Observable<FederalDistrict[]> {
+    return this.federalDistricts$.pipe(
+      map(x => x.slice(-this.NFederalDistricts))
+    );
+  }
+
+  getFederalDistrictsTimeLine(): Observable<FederalDistrict[]> {
     return this.federalDistricts$;
   }
 
