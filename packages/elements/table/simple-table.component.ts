a519a66c35601f3154ef17322a0a10ab5e279a66
zwarag <harrys.kavan@protonmail.com>
2020-08-28 12:30:30 +0200

    feat(simpletable): make it dynamic

--- packages/elements/table/simple-table.component.ts
+++ packages/elements/table/simple-table.component.ts
@@ -1,21 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { GenericTable, Global } from './data-source.interface';
-import { Observable } from 'rxjs';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-simple-table',
   templateUrl: './simple-table.component.html',
   styleUrls: ['./simple-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
-
 })
-export class SimpleTableComponent implements OnInit {
-
-  @Input() tableData$: Observable<GenericTable<unknown, unknown>>;
-
-  ngOnInit(): void {
-    // this.tableData$?.subscribe({
-    //   next:r => console.log('lib', r)
-    // })
-  }
+export class SimpleTableComponent {
+  @Input() title: string
+  @Input() tableHeads: unknown
+  @Input() tableRows: unknown
 }
--- packages/frontend/src/app/shared/components/data-table/data-table.component.ts
+++ packages/frontend/src/app/shared/components/data-table/data-table.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs';
 import { GenericTable } from '@lib/elements/table';
@@ -6,16 +6,29 @@ import { GenericTable } from '@lib/elements/table';
 @Component({
   selector: 'app-data-table',
   template: `
-    <lib-simple-table [tableData$]="dataSource$"></lib-simple-table>`,
+    <lib-simple-table
+      [title]="_title"
+      [tableHeads]="_tableHeads"
+      [tableRows]="_tableRows">
+    </lib-simple-table>`,
   exportAs: 'appDataTable',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default
 })
+
 // @ts-ignore
-export class DataTableComponent {
+export class DataTableComponent implements OnInit {
   @Input() dataSource$: Observable<GenericTable<unknown, unknown>>;
-  // ngOnInit(): void {
-  // this.dataSource$.subscribe({
-  //   next: res => console.log('shared',res)
-  // })
-  // }
+  _title: string
+  _tableHeads: unknown
+  _tableRows: unknown
+
+  ngOnInit(): void {
+    this.dataSource$.subscribe({
+      next: res => {
+        this._title = res.title
+        this._tableHeads = res.tableHeads
+        this._tableRows = res.tableRows
+      }
+    })
+  }
 }
